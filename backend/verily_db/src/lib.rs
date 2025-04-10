01139214a696fd745d12ddb0d057897863c9bbe9
Ifiok Jr. <ifiokotung@gmail.com>
2025-04-10 08:16:07 +0100

    feat: remove rust cos nope

--- backend/verily_db/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(feature = "ssr")]
-pub use db::*;
-#[cfg(feature = "ssr")]
-pub use ssr_utils::*;
-
-#[cfg(feature = "ssr")]
-mod db;
-#[cfg(feature = "ssr")]
-mod ssr_utils;
--- packages/verily_rpc/example/android/app/src/main/kotlin/com/example/verily_rpc_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.verily_rpc_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
