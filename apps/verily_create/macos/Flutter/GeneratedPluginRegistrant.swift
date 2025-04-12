c7cdbc26243f6aa82c8b6afe53e1230cb3855de9
Ifiok Jr. <ifiokotung@gmail.com>
2025-04-12 13:02:28 +0100

    feat(icon_management): update app icons and integrate flutter_launcher_icons
    - Added flutter_launcher_icons dependency to manage app icons for Android, iOS, and web.
    - Configured icon generation settings in pubspec.yaml for both verily and verily_create apps.
    - Updated AndroidManifest.xml to reference the new launcher icon.
    - Simplified iOS app icon configuration by consolidating icon assets into a single JSON file.
    - Updated web manifest to reflect new background and theme colors.
    - Added new icon assets for various resolutions and platforms.

--- apps/verily_create/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/verily_create/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,13 @@ import Foundation
 import connectivity_plus
 import file_picker
 import file_selector_macos
+import firebase_analytics
+import firebase_app_check
+import firebase_app_installations
+import firebase_auth
+import firebase_core
+import firebase_crashlytics
+import firebase_messaging
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
@@ -16,6 +23,13 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
+  FirebaseInstallationsPlugin.register(with: registry.registrar(forPlugin: "FirebaseInstallationsPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
