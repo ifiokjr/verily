f39a903d84d6149856469ce8bd04dd783e274a99
Ifiok Jr. <ifiokotung@gmail.com>
2025-04-13 12:43:29 +0100

    fix: update speech_to_text plugin import for compatibility
    - Changed the import statement for the speech_to_text plugin from 'speech_to_text_macos' to 'speech_to_text' to ensure compatibility across platforms.
    - Updated the registration of the SpeechToTextPlugin in the RegisterGeneratedPlugins function.

--- apps/verily_create/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/verily_create/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -18,7 +18,7 @@ import firebase_messaging
 import geolocator_apple
 import path_provider_foundation
 import shared_preferences_foundation
-import speech_to_text_macos
+import speech_to_text
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -35,6 +35,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
